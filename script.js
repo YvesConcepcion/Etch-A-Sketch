a65998b91f13249d212a258227d05dc1fbfd573f
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2023-03-20 21:07:29 +0800

    Added color buttons and eraser

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-"use strict mode";
+"use strict";
 
+//Element
 const sketchContainer = document.querySelector(".sketch-container");
-
+const btnBlack = document.querySelector(".black");
+const btnRed = document.querySelector(".red");
+const btnGreen = document.querySelector(".green");
+const btnBlue = document.querySelector(".blue");
+const btnEraser = document.querySelector(".eraser");
+let color = "black";
 function createGrid(gridAmount) {
-  for (let i = 1; i < gridAmount; i++) {
+  for (let i = 0; i < gridAmount; i++) {
     const row = document.createElement("div");
+    row.style.width = "100%";
+    row.style.height = `${100 / gridAmount}%`;
     sketchContainer.appendChild(row);
 
     for (let j = 0; j < gridAmount; j++) {
       const square = document.createElement("div");
-      square.setAttribute("id", "innerSquare");
       square.classList.add("square");
-      square.style.width = `${600 / gridAmount}px`;
-      square.style.height = `${600 / gridAmount}px`;
+      square.style.width = `${100 / gridAmount}%`;
+      square.style.height = "100%";
+      square.style.backgroundColor = "white";
       row.appendChild(square);
     }
   }
 }
 
 createGrid(16);
+
+const changeColor = function (event) {
+  let square = event.target;
+  square.style.backgroundColor = color;
+};
+
+//Buttons
+btnBlack.onclick = function () {
+  color = "black";
+};
+btnRed.onclick = function () {
+  color = "red";
+};
+btnGreen.onclick = function () {
+  color = "green";
+};
+btnBlue.onclick = function () {
+  color = "blue";
+};
+
+btnEraser.onclick = function () {
+  color = "white";
+};
+
+sketchContainer.addEventListener("mouseover", changeColor);
